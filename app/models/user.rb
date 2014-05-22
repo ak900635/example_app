9d88c525a5ef338a6480a9f9c33fb26b4fa4a2f1
L3I <arifa.karim>
2014-05-22 04:27:44 -0700

    commit Initial tp note

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
 
 before_save { |user| user.email = email.downcase }
+before_create :create_remember_token
 validates :name, presence: true, length: { maximum:50 }
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true, format:{ with: VALID_EMAIL_REGEX },
@@ -12,3 +13,19 @@ validates :password , presence: true, length: { minimum: 6 }
 validates_confirmation_of :password,
                           if: lambda { |m| m.password.present? }
 end
+ def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
+
+
+
